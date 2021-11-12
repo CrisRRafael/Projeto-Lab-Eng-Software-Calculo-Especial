7e3fa163aff646e61bf6a545130ad624f4c48296
CrisRRafael <cfrabc@gmail.com>
2021-11-12 19:56:42 -0300

    Correção do calculo raiz

--- calc_esp.py
+++ calc_esp.py
@@ -11,12 +11,15 @@ CORS(app)
 @app.route('/calc_raiz')
 
 def calcular_raiz():
-   valores = request.args.get('str_input')
-   indice= float(valores[0])
-   radicando = float(valores[1])
-   resultado = radicando **(1/indice)
-   str_output = {'resultado': resultado}
-   return jsonify(str_output)
+   data = request.args.get('str_input')
+   valores=data.split(',')
+   print(valores)
+   indice= int(valores[0])
+   radicando = int(valores[1])
+   resultado = float(radicando **(1/indice))
+   str_raiz = {'resultado': resultado}
+   print(str_raiz)   
+   return jsonify(str_raiz)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5004)
