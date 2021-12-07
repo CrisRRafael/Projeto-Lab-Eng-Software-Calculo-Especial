331f3f07a888c31e871d22e30ea62ee568b75c05
CrisRRafael <cfrabc@gmail.com>
2021-12-06 21:02:00 -0300

    Teste raiz enesima correto

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ class Test_calc(unittest.TestCase):
     def test_sub(self): 
         result =func("3,27")
         print(result)
-        self.assertEqual(result, {'resultado':4.0})
+        self.assertEqual(result, {'resultado':3.0})
 
 if __name__ == '__main__':
     unittest.main()
